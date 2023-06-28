8efa0cb21366b2911b2d56787dc8a6f1c8722c3b
alirezaf2001 <68161296+alirezaf2001@users.noreply.github.com>
2023-06-28 18:18:56 +0330

    initial chess

---
+++ chess.py
@@ -0,0 +1,70 @@
+class Chess:
+    def __init__(self,location , white):
+        self.location = location
+        self.white = white
+
+    def move(self, destination):
+
+
+class Pawn(Chess):
+    """
+    Pawn can only move forward and attack forward-diagonally
+    """
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return 'P'
+
+class Bishop(Chess):
+    """
+    Bishop can only move diagonally
+    """
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return 'B'
+
+class Knight(Chess):
+    """
+    Knight can only move in L shape and can jump pieces
+    """
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return 'K'
+
+
+class Rook(Chess):
+    """
+    Rook can only move in straight line
+    """
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return 'R'
+
+
+class Queen(Chess):
+    """
+    Queen can move in straight lines or diagonally
+    """
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return 'Q'
+
+
+class King(Chess):
+    """
+    King can only move one space in each direction
+    """
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return 'K'
