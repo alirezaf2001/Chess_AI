7a4a169ae12845f58185928a87eeec8588e65d40
alirezaf2001 <68161296+alirezaf2001@users.noreply.github.com>
2023-06-28 21:57:42 +0330

    add validation movement to Base class and delete __init__ from children and override validation movement function in each child

--- chess.py
+++ chess.py
@@ -1,17 +1,55 @@
 class Chess:
-    def __init__(self,location , white):
-        self.location = location
-        self.white = white
+    def __init__(self, location, color):
+        self.x = location[0]
+        self.y = location[1]
+        self.color = color
+
+    def validate_movement(self):
+        pass
 
     def move(self, destination):
-        pass
+        if self.validate_movement(destination):
+            self.x = destination[1]
+            self.y = destination[0]
+            return True
+        else:
+            return False
+
 
 class Pawn(Chess):
     """
     Pawn can only move forward and attack forward-diagonally
     """
-    def __init__(self):
-        pass
+    def __init__(self, location, color):
+        super().__init__(location, color)
+        self.first_move = True
+
+    def validate_movement(self, destination):
+        # TODO: Finish pawn movement validation
+        if self.color == "white":
+            if self.first_move:
+                if 0 < self.x - destination[1] <= 2 and self.y == destination[0]:
+                    return True
+            else:
+                if self.x - destination[1] == 1 and self.y == destination[0]:
+                    return True
+        else:
+            if self.first_move:
+                if 0 < destination[1] - self.x <= 2 and self.y == destination[0]:
+                    return True
+            else:
+                if destination[1] - self.x == 1 and self.y == destination[0]:
+                    return True
+        return False
+
+    def move(self, destination):
+        if self.validate_movement(destination):
+            self.x = destination[1]
+            self.y = destination[0]
+            self.first_move = False
+            return True
+        else:
+            return False
 
     def __repr__(self):
         return 'P'
@@ -20,8 +58,8 @@ class Bishop(Chess):
     """
     Bishop can only move diagonally
     """
-    def __init__(self):
-        pass
+    def validate_movement(self, destination):
+        return True
 
     def __repr__(self):
         return 'B'
@@ -30,8 +68,8 @@ class Knight(Chess):
     """
     Knight can only move in L shape and can jump pieces
     """
-    def __init__(self):
-        pass
+    def validate_movement(self, destination):
+        return True
 
     def __repr__(self):
         return 'K'
@@ -41,8 +79,8 @@ class Rook(Chess):
     """
     Rook can only move in straight line
     """
-    def __init__(self):
-        pass
+    def validate_movement(self, destination):
+        return True
 
     def __repr__(self):
         return 'R'
@@ -52,8 +90,8 @@ class Queen(Chess):
     """
     Queen can move in straight lines or diagonally
     """
-    def __init__(self):
-        pass
+    def validate_movement(self, destination):
+        return True
 
     def __repr__(self):
         return 'Q'
@@ -63,8 +101,8 @@ class King(Chess):
     """
     King can only move one space in each direction
     """
-    def __init__(self):
-        pass
+    def validate_movement(self, destination):
+        return True
 
     def __repr__(self):
-        return 'K'
+        return 'KING'
